1c727b85d67b0d545950656595ead2c60ad1bdf4
ankur-dengla-glean <ankur.dengla@glean.com>
2024-01-24 21:40:07 +0530

    Added PR Check

--- extract.sh
+++ extract.sh
@@ -1,20 +1,28 @@
-input_str="/trigger-workflow pr_check" 
-command_name="/trigger-workflow"
+# input_str="/trigger-workflow pr_check" 
+# command_name="/trigger-workflow"
 
-# Remove the leading ‘\trigger-workflow ’
-command_params=${input_str#$command_name}
-echo $command_params
+# # Remove the leading ‘\trigger-workflow ’
+# command_params=${input_str#$command_name}
+# echo $command_params
 
-IFS=' ' read -ra args <<< $command_params
+# IFS=' ' read -ra args <<< $command_params
 
-workflow_name=${args[0]}
-echo "workflow_name=${workflow_name}" 
+# workflow_name=${args[0]}
+# echo "workflow_name=${workflow_name}" 
 
-for ((i = 1; i < ${#args[@]}; i++)); do
-    arg=${args[i]}
-    if [[ $arg == --* ]]; then
-        key=${arg#--}
-    else
-        echo "${key}=${arg}" 
-    fi
-done
+# for ((i = 1; i < ${#args[@]}; i++)); do
+#     arg=${args[i]}
+#     if [[ $arg == --* ]]; then
+#         key=${arg#--}
+#     else
+#         echo "${key}=${arg}" 
+#     fi
+# done
+
+
+if  false ; then
+      echo "Contains"
+  else
+      echo "Does not contain"
+  fi
+  
